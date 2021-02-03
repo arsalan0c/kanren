6c767882dc588fd7c28d0bf1966cbe1c8916cd94
Arsalan Cheema <arsalanc.v2@gmail.com>
2021-02-03 22:16:31 +0800

    clean

--- examples/main.ml
+++ examples/main.ml
@@ -2,25 +2,40 @@ open Mk
 
 let printf = Stdlib.Printf.printf
 
-let rec fives_or_sixes x = disj ((===) x (Atom 5)) (fun sc -> Immature (fun () -> fives_or_sixes x sc))
-let two_fives = take 2 (call_fresh (fun x -> fives x) empty_state)
+let rec fives x = disj ((===) x (Atom (Int 5))) (fun sc -> Immature (fun () -> fives x sc))
+let three_fives = take 3 (call_fresh (fun x -> fives x) empty_state)
 
-let one_five x = (once ((===) (Atom (Int 5)) (Atom 5))) empty_state
+let one_five = (once ((===) (Atom (Int 6)) (Atom (Int 5)))) empty_state
 
-let a_and_b = conj (call_fresh (fun x -> (===) x (Atom 7))) (call_fresh (fun x -> disj ((===) x (Atom 5)) ((===) x (Atom 6)))) empty_state
+let a_and_b = conj (call_fresh (fun x -> (===) x (Atom (Int 7)))) (call_fresh (fun x -> disj ((===) x (Atom (Int 5))) ((===) x (Atom (Int 6))))) empty_state
 
-let fivesix_or_seven x y =  ifte ((===) x (Atom 5)) ((===) y (Atom 6))  ((===) x (Atom 7))
+let fivesix_or_seven x y = ifte ((===) x (Atom (Int 5))) ((===) y (Atom (Int 6))) ((===) x (Atom (Int 7)))
+let els x = ifte ((===) (Atom (Int 5)) (Atom (Int 6))) ((===) x (Atom (Int 6))) ((===) x (Atom (Int 7)))
+
 
 (* if resulting stream has no states, the formula is unsatisfiable
   otherwise, return one result after another
 *)
-let p = call_fresh (fun x -> disj ((===) x (Atom 1)) ((===) x (Atom 0)))
-let q = call_fresh (fun x -> disj ((===) x (Atom 1)) ((===) x (Atom 0)))
-let r = call_fresh (fun x -> disj ((===) x (Atom 1)) ((===) x (Atom 0)))
+let p = call_fresh (fun x -> conj ((===) x (Atom (Int 1))) ((===) x (Atom (Int 0))))
+let q = call_fresh (fun x -> disj ((===) x (Atom (Int 1))) ((===) x (Atom (Int 0))))
+let r = call_fresh (fun x -> disj ((===) x (Atom (Int 1))) ((===) x (Atom (Int 0))))
 
-let formula = (===) (conj_plus [(disj p q); r]) (Atom 1)
+let f1 = disj_plus [p; q]
+let f2 = conj p q
+let f3 = disj_plus [(call_fresh (fun x -> fives x)); q; r]
 
-let g = call_empty_state formula
+(* (===) (Atom (Int 1)) (Atom (Int 1)) *)
+(* (conj_plus [(disj p q); r]) *)
+
+let g = call_empty_state f1
+let g2 = call_empty_state (call_fresh (fun x -> call_fresh (fun y -> fivesix_or_seven x y)))
+let g3 = call_empty_state (call_fresh (fun x -> els x))
+
+let g4 = call_empty_state (fresh3 (fun (x, y, z) -> disj_plus [((===) x (Atom (Str "first"))); ((===) y (Atom (Str "sec"))); ((===) z (Atom (Str "third")))]))
+
+let at i lst = List.nth lst i 
+let g5 = call_empty_state (freshN 3 (fun vars -> conj_plus [((===) (at 0 vars) (Atom (Int 1))); ((===) (at 1 vars) (Atom (Int 2))); ((===) (at 2 vars) (Atom (Int 3)))]))
+
 
 
 let () = begin
@@ -32,7 +47,7 @@ let () = begin
       | Some x -> printf "\n%s\n" (state_to_string x)
       | None -> printf "\n%s\n" "No state" *)
    
-    printf "\n%s\n" (stream_to_string g)
+    printf "\n%s\n" (stream_str g3)
       (* match res with 
       | Cons(a, _) -> printf "\n%s\n" (state_to_string a)
       | _ -> printf "\n%s\n" "other" *)
--- lib/mk.ml
+++ lib/mk.ml
@@ -3,17 +3,13 @@ open Base
 exception Failure of string
 let[@inline] failwith msg = raise (Failure ("Failure: " ^ msg))
 
-(* 
-    It seems type declarations have to be duplicated from the .mli by default which isn't ideal,
-    although there are workarounds.
-    See https://discuss.ocaml.org/t/why-do-i-need-to-repeat-type-declarations-between-interfaces-and-implementations-or-how-do-i-get-around-this/3350/7
-*)
+let printf = Stdlib.Printf.printf
 
 type var = int
 type var_counter = int
-type atom = Int of int | Float of float | Bool of bool | String of string 
-type term = Var of var | Pair of var * term | Atom of atom
 type 'a stream = Nil | Immature of (unit -> 'a stream) | Cons of 'a * ('a stream)
+type atom = Int of int | Float of float | Bool of bool | Str of string
+type term = Atom of atom | Var of var | Pair of var * term
 
 type substitution = (int, term, Int.comparator_witness) Map.t 
 type state = substitution * var_counter
@@ -22,7 +18,18 @@ type goal = state -> state stream
 let mZero = Nil
 let empty_state = (Map.empty (module Int), 0)
 
-let rec walk t (s: substitution) = match t with
+let eqv (u: atom) (v: atom) = 
+    let atom_compare u v =
+        match u, v with
+        | Int x, Int y -> Int.compare x y
+        | Float x, Float y -> Float.compare x y
+        | Bool x, Bool y -> Bool.compare x y
+        | Str x, Str y -> String.compare x y
+        | _, _ -> -1
+    in
+    atom_compare u v = 0
+
+let rec walk t s = match t with
     | Var v -> begin
         match Map.find s v with
         | Some x -> walk x s
@@ -36,24 +43,27 @@ let rec occurs v t s =
     | Pair(e1, e2) -> (occurs v (Var e1) s) || (occurs v e2 s)
     | _ -> false 
 
-let ext_s v t s = if occurs v t s 
+let ext_s v t s = 
+    if occurs v t s 
     then failwith ("circularity in substitution with variable:" ^ (Int.to_string v)) 
     else Map.set s ~key:v ~data:t
 
 let rec unify u v s = 
     match walk u s, walk v s with
-    | Var(e1), Var(e2) when e1 = e2 -> Some s
-    | Var(e), z -> Some (ext_s e z s)
-    | z, Var(e) -> Some (ext_s e z s)
+    | Var e1, Var e2 when e1 = e2 -> Some s
+    | Var e, z -> Some (ext_s e z s)
+    | z, Var e -> Some (ext_s e z s)
     | Pair(x1, y1), Pair(x2, y2) -> begin 
         match unify (Var x1) (Var x2) s with
         | Some s2 -> unify y1 y2 s2
         | None -> None
         end
+    | Atom e1, Atom e2 when eqv e1 e2 -> Some s
     | _, _ -> None
 
+    
 
-(* Goals *)
+(* Core μKanren goals *)
 
 let (===) u v = 
     fun sc ->
@@ -63,56 +73,66 @@ let (===) u v =
 
 let call_fresh (f: term -> goal) =
     fun sc -> f (Var (snd sc)) (fst sc,(snd sc)+1)
-
-let call_empty_state g = g empty_state 
-
+        
 let rec mplus s1 s2 = match s1, s2 with
     | Nil, _ -> s2
-    | Immature(f1), Immature(f2) -> Immature(fun () -> mplus (f1()) (f2()))
-    | Immature(f), _ -> Immature(fun () -> mplus s2 (f())) 
-    | Cons(a, ss), _ -> Cons(a, mplus s2 ss)
+    | Immature f1, Immature f2 -> Immature(fun () -> mplus (f1()) (f2()))
+    | Immature f, _ -> Immature(fun () -> mplus s2 (f())) 
+    | Cons(a, r), _ -> Cons(a, mplus s2 r)
 
 let disj g1 g2 =
     fun sc -> mplus (g1 sc) (g2 sc)
 
 let rec bind s g = match s with
     | Nil -> mZero
-    | Immature(f) -> Immature(fun () -> bind (f()) g)
-    | Cons(a, ss) -> mplus (g a) (bind ss g)
+    | Immature f -> Immature(fun () -> bind (f()) g)
+    | Cons(a, r) -> mplus (g a) (bind r g)
 
 let conj g1 g2 =
     fun sc -> bind (g1 sc) g2   
 
-(* Not a must, but delay each goal so that if a user is only using disj_plus or conj_plus, they don't have to manually delay *)
-let rec disj_plus gs = match List.hd gs, List.tl gs with
-    | Some(g), Some(t) -> disj (fun sc -> Immature(fun () -> g sc)) (disj_plus t)
-    | Some(g), _ -> fun sc -> Immature(fun () -> g sc)
-    | _, _ -> fun _ -> mZero 
-
-let rec conj_plus gs = match List.hd gs, List.tl gs with
-    | Some(g), Some(t) -> conj (fun sc -> Immature(fun () -> g sc)) (conj_plus t)
-    | Some(g), _ -> fun sc -> Immature(fun () -> g sc)
-    | _, _ -> fun _ -> mZero
-
-let conde gss = disj_plus (List.map gss ~f:conj_plus)
 
 
-(* Functions for convenience *)
+(* Additions *)
 
+let call_empty_state g = g empty_state 
+
+let disj_plus gs = 
+    match List.reduce gs ~f:disj with
+    | Some g -> fun sc -> g sc
+    | None -> fun _ -> mZero 
+
+let conj_plus gs = 
+    match List.reduce gs ~f:conj with
+    | Some g -> fun sc -> g sc
+    | None -> fun _ -> mZero
+
+(* force evaluation of a stream *)
 let rec pull s = match s with
-    | Immature(f) -> pull(f())
+    | Immature f -> pull(f())
     | _ -> s
 
-let rec take n s = match n, s with
-    | 0, _ -> mZero
-    | _, s -> match pull s with
-        | Cons(a, s) -> Cons(a, take (n - 1) s)
-        | _ -> mZero
+let rec take n s =
+    if n <= 0 then mZero else 
+        begin
+            match pull s with
+            | Cons(a, r) -> Cons(a, take (n - 1) r)
+            | _ -> mZero
+        end
 
 let rec take_all s = match pull s with
-    | Cons(a, s) -> Cons(a, take_all s)
+    | Cons(a, r) -> Cons(a, take_all r)
     | _ -> mZero
 
+let call_initial_state n g =
+    take n (pull (call_empty_state g))
+
+(* trivial reifier *)
+let rec length s = match pull s with
+    | Cons(_, r) -> 1 + length r
+    | _ -> 0
+
+(* committed choice *)
 let once g =
     fun sc -> 
         let rec loop s = begin 
@@ -123,7 +143,8 @@ let once g =
         end in
         loop (g sc)
 
-let ifte g1 g2 g3 = 
+(* soft-cut *)
+let ifte g1 g2 g3 =  
     fun sc -> 
         let rec loop s g2 g3 = begin 
             match s with
@@ -133,18 +154,38 @@ let ifte g1 g2 g3 =
             end in
         loop (g1 sc) g2 g3
 
-let call_initial_state n g =
-    take n (pull (call_empty_state g))
+(* disjunction of conjunctions *)
+let conde (gss: (goal list) list) = 
+    fun sc -> 
+        disj_plus (List.map gss ~f:conj_plus) sc
 
+let fresh2 (f: term * term -> goal) =
+    fun sc ->
+        let counter = snd sc in
+        f ((Var counter), (Var (counter+1)))
+            (fst sc, counter+2) 
+    
+let fresh3 (f: term * term * term -> goal) =
+    fun sc ->
+        let counter = snd sc in
+        f ((Var counter), (Var (counter+1)), (Var (counter+2)))
+            (fst sc, counter+3) 
 
+let freshN n (f: term list -> goal) =
+        fun sc ->
+            let counter = snd sc in
+            let vars = List.map (List.range counter (counter + n)) ~f:(fun i -> Var i) in
+            f vars (fst sc, counter+n) 
+
+            
 (* Functions for converting to string *)
 
 let atom_str a =
     match a with
-    | Int(x) -> Int.to_string x
-    | Float(x) -> Float.to_string x
-    | Bool(x) -> Bool.to_string x
-    | String(x) -> x
+    | Int x -> Int.to_string x
+    | Float x -> Float.to_string x
+    | Bool x -> Bool.to_string x
+    | Str x -> x
 
 let rec term_str t = 
     match t with
@@ -153,16 +194,21 @@ let rec term_str t =
     | Atom(x) -> atom_str x
 
 let subst_str subst =
-    "[" ^ (Map.fold subst ~init:"" ~f:(fun ~key:k ~data:v acc -> 
+    "substitution: [" ^ (Map.fold subst ~init:"" ~f:(fun ~key:k ~data:v acc -> 
         let k_string = Int.to_string k in
         let v_string = term_str v in
         acc ^ "(" ^ k_string ^ ", " ^ v_string ^ ")" ^ ",  " 
     )) ^ "]"
 
 let state_str (subst, counter) =
-    "{" ^ (subst_str subst) ^ ", " ^ (Int.to_string counter) ^ "}"
+    "{ " ^ (subst_str subst) ^ ", counter: " ^ (Int.to_string counter) ^ " }"
 
-let rec stream_str s = match s with
+let rec stream_str_h s = 
+    match s with
     | Nil -> ""
-    | Immature(f) -> stream_str (f())
-    | Cons(a, s) -> state_str a ^ "\n" ^ stream_str s
+    | Immature(f) -> stream_str_h (f())
+    | Cons(a, r) -> state_str a ^ "\n" ^ stream_str_h r
+
+let stream_str s = 
+    "States:\n" ^ (stream_str_h s)
+            
